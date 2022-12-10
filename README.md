28d912e70c90d2c7230c7a0ccaf3167c07187a8d
lamoraal <lamoraaleland@gmail.com>
2022-12-10 15:57:35 +0100

    Plan the project, add comments

---
+++ README.md
@@ -0,0 +1,18 @@
+#Mixed messages project
+
+###Project name tbd
+
+This project will return random messages to the user every time the program is run.
+Now is the time to come up with an idea of what to make it about
+
+####Ideas
++ inspirational quotes
++ a tip for a book, a movie and a song
+Yes, I like this last one.
+The message can be as following :
+**I have peered into your soul and these are my suggestions :
+**You should watch *movietitle* before the week is over.
+
+**The song your soul is craving for now is *songtitle by artist*.
+
+**To make a profound change in your life, you should read *booktitle by author*.
---
+++ main.js
@@ -0,0 +1,29 @@
+//the project will give the user tips for a movie, a song and a book
+
+// First we'll create three arrays with data
+
+//Array 1 : movietitles, try to get at least thirty
+
+//Array 2 : songtitles, get at least forty
+
+//Array 3 : booktitles, get at least twenty
+
+//function to get a random number
+//parameter will be a number (the length of one of the three arrays)
+//return will be a random number
+
+
+
+
+//function to get a random entry from an array
+//parameter will be an array
+//this function will use the function above
+//return will be a random entry
+
+
+
+//function to make up the full message
+//this will go through three steps, adding to the message that will be outputted to the user
+//Put the three messages all in this one function, or make three extra smaller functions.
+//no parameters
+//return will be the mixed message
